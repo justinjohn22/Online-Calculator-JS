1102bc60b2f19bbff5afe68b7d9775eb74bbb118
Justin John (s3846390) <s3846390@student.rmit.edu.au>
2021-07-30 20:01:56 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// https://www.youtube.com/watch?v=j59qQ7YWLxw&ab_channel=WebDevSimplified
+// at 17:58
+
+class Calculator {
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+  }
+  clear() {
+
+  }
+  delete() {
+
+  }
+  appendNumber() {
+
+  }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
